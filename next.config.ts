f2b08878a03111317d11bb576278f3e11284d85c
bimal009 <pandeybimal616@gmail.com>
2025-05-10 14:52:52 +0545

    refactor: comment out ngrok URL in allowedDevOrigins for clarity

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig = {
   // Otherwise remove it to avoid confusion
   allowedDevOrigins: [
     // 'https://buntly.vercel.app',
-    "https://871c-2400-1a00-bb20-a64f-b56c-a83b-44c9-533b.ngrok-free.app",
+    // "https://871c-2400-1a00-bb20-a64f-b56c-a83b-44c9-533b.ngrok-free.app",
 
     //  // Remove trailing slash for consistency
   ],
