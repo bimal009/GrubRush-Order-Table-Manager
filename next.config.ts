34cb33c525770a85bbf049804720b1ce575ca4dc
bimal009 <pandeybimal616@gmail.com>
2025-05-10 15:08:28 +0545

    refactor: update allowedDevOrigins for consistency and clarity

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,9 @@ const nextConfig = {
   // Only include this if your app depends on this key
   // Otherwise remove it to avoid confusion
   allowedDevOrigins: [
-    // 'https://buntly.vercel.app',
+    'https://buntlynp.vercel.app',
     // "https://871c-2400-1a00-bb20-a64f-b56c-a83b-44c9-533b.ngrok-free.app",
 
-    //  // Remove trailing slash for consistency
   ],
   images: {
     remotePatterns: [
